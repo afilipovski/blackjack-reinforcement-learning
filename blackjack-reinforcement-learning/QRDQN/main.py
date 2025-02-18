0171d58d369f0363e44ad0884b9bbef8faa5afea
Aleksandar Filipovski <aleksandar.filipovski@codechem.com>
2025-02-18 20:14:59 +0100

    add new qrdqn env

---
+++ blackjack-reinforcement-learning/QRDQN/main.py
@@ -0,0 +1,60 @@
+import gymnasium as gym
+from gymnasium.spaces import Discrete
+from sb3_contrib import QRDQN
+
+class BlackjackObsWrapper(gym.ObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.observation_space = Discrete(32 * 11 * 2)
+
+    def observation(self, obs):
+        player_sum, dealer_card, usable_ace = obs
+        return (player_sum * 11 * 2) + (dealer_card * 2) + usable_ace
+
+env = BlackjackObsWrapper(gym.make("Blackjack-v1"))
+
+model = QRDQN(
+    "MlpPolicy",
+    env,
+    learning_rate=0.0064935100500415335,
+    buffer_size=240000,
+    learning_starts=1000,
+    batch_size=128,
+    gamma=0.55,
+    train_freq=13,
+    target_update_interval=3000,
+    exploration_fraction=0.0004291352471643776,
+    exploration_final_eps=0.01,
+    exploration_initial_eps=1,
+    verbose=1,
+)
+
+if input("Train?") == "Y":
+    # name = input("enter the name of the agent (default=None). ")
+    # if name:
+        # model = QRDQN.load(name, env)
+    model.learn(total_timesteps=100000)
+    # if not name:
+    name = input("name the agent. ")
+    model.save(name)
+
+if input("Test?") == "Y":
+    results = []
+
+    name = input("enter the name of the agent. ")
+    model = QRDQN.load(name)
+    for i in range(10000):
+        obs, _ = env.reset()
+        done = False
+        obs, reward, done = 0,0,0
+        reward_sum = 0
+        while not done:
+            action, _ = model.predict(obs)
+            obs, reward, done, _, _ = env.step(action)
+            reward_sum += reward
+            print(f"Action: {action}, Reward: {reward}")
+        results.append(reward_sum)
+    print(len([r for r in results if r==1.5]))
+    print(len([r for r in results if r==1]))
+    print(len([r for r in results if r==0]))
+    print(len([r for r in results if r==-1]))
---
+++ blackjack-reinforcement-learning/QRDQN/optimize.py
@@ -0,0 +1,54 @@
+import optuna
+import gymnasium as gym
+from gymnasium.spaces import Discrete
+from sb3_contrib import QRDQN
+from stable_baselines3.common.evaluation import evaluate_policy
+
+class BlackjackObsWrapper(gym.ObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.observation_space = Discrete(32 * 11 * 2)
+
+    def observation(self, obs):
+        player_sum, dealer_card, usable_ace = obs
+        return (player_sum * 11 * 2) + (dealer_card * 2) + usable_ace
+
+def objective(trial):
+    env = BlackjackObsWrapper(gym.make("Blackjack-v1"))
+
+    # Hyperparameter search space
+    learning_rate = trial.suggest_loguniform("learning_rate", 1e-5, 1e-2)
+    buffer_size = trial.suggest_int("buffer_size", 10_000, 500_000, step=10_000)
+    batch_size = trial.suggest_categorical("batch_size", [32, 64, 128, 256])
+    gamma = trial.suggest_float("gamma", 0.1, 0.99, step=0.05)
+    train_freq = trial.suggest_int("train_freq", 1, 20)
+    target_update_interval = trial.suggest_int("target_update_interval", 500, 5000, step=500)
+    exploration_fraction = trial.suggest_float("exploration_fraction", 0.0001, 0.1, log=True)
+    exploration_final_eps = trial.suggest_float("exploration_final_eps", 0.01, 0.1, step=0.01)
+
+    model = QRDQN(
+        "MlpPolicy",
+        env,
+        learning_rate=learning_rate,
+        buffer_size=buffer_size,
+        batch_size=batch_size,
+        gamma=gamma,
+        train_freq=train_freq,
+        target_update_interval=target_update_interval,
+        exploration_fraction=exploration_fraction,
+        exploration_final_eps=exploration_final_eps,
+        verbose=0,
+    )
+
+    model.learn(total_timesteps=5000)  # Train the model
+    mean_reward, _ = evaluate_policy(model, env, n_eval_episodes=100)  # Evaluate performance
+
+    return mean_reward  # Higher reward is better
+
+# Run Optuna optimization
+study = optuna.create_study(direction="maximize")
+study.optimize(objective, n_trials=15)  # Run 50 trials
+
+# Print best parameters
+print("Best parameters:", study.best_params)
+pass
